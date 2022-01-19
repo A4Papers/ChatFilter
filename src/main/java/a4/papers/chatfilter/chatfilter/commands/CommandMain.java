0794295395b420e841c029e3f098fdee5f8315fa
BuildTools <unconfigured@null.spigotmc.org>
2022-01-19 19:22:07 +1100

    Cleaned up commands

---
+++ src/main/java/a4/papers/chatfilter/chatfilter/commands/CommandMain.java
@@ -0,0 +1,62 @@
+package a4.papers.chatfilter.chatfilter.commands;
+
+import a4.papers.chatfilter.chatfilter.ChatFilter;
+import a4.papers.chatfilter.chatfilter.lang.EnumStrings;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandMain implements CommandExecutor {
+
+    private ChatFilter chatFilter;
+    private ClearChatCommand clearChatCommand;
+    private ReloadCommand reloadCommand;
+    private HelpCommand helpCommand;
+
+    private PauseCommand pauseCommand;
+    private BlacklistCommand blacklistCommand;
+    private WhitelistCommand whitelistCommand;
+
+
+    public CommandMain(ChatFilter chatFilter) {
+        this.chatFilter = chatFilter;
+        this.clearChatCommand = new ClearChatCommand(this.chatFilter);
+        this.reloadCommand = new ReloadCommand(this.chatFilter);
+        this.helpCommand = new HelpCommand(this.chatFilter);
+
+        this.pauseCommand = new PauseCommand(this.chatFilter);
+        this.blacklistCommand = new BlacklistCommand(this.chatFilter);
+        this.whitelistCommand = new WhitelistCommand(this.chatFilter);
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length >= 1) {
+            switch (args[0].toLowerCase()) {
+                case "clear":
+                    this.clearChatCommand.onCommand(sender, cmd, label, args);
+                    break;
+                case "reload":
+                    this.reloadCommand.onCommand(sender, cmd, label, args);
+                    break;
+                case "pause":
+                    this.pauseCommand.onCommand(sender, cmd, label, args);
+                    break;
+                case "help":
+                    this.helpCommand.onCommand(sender, cmd, label, args);
+                    break;
+                case "blacklist":
+                    this.blacklistCommand.onCommand(sender, cmd, label, args);
+                    break;
+                case "whitelist":
+                    this.whitelistCommand.onCommand(sender, cmd, label, args);
+                    break;
+                default:
+                    sender.sendMessage(chatFilter.colour(chatFilter.mapToString(EnumStrings.NO_ARGS.s)));
+            }
+        } else {
+            sender.sendMessage(chatFilter.colour(chatFilter.mapToString(EnumStrings.NO_ARGS.s)));
+        }
+        return true;
+    }
+}
